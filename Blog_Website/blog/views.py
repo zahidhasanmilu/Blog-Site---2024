3a9494f46344d8a25c5aa3154774bd31c627d217
zahidhasanmilu <minions.milu889@gmail.com>
2024-03-26 04:59:59 +0600

    first commit

---
+++ Blog_Website/blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, redirect, HttpResponse
+from django.urls import reverse, reverse_lazy
+
+# VIEW
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, View, TemplateView, DeleteView
+
+# Authentication
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+
+# forms
+from django.contrib.auth.forms import AuthenticationForm
+
+# models
+from App_account.models import User, Profile
+from App_shop.models import Product, Category,ProductImage
+
+# forms
+from App_account.forms import SignUpForm
+
+# message
+from django.contrib import messages
+
+import uuid
+from django.db.models import Q
+
+
+######################## Create your views here. #############################
+def Home(request):
+    return render(request, '')
